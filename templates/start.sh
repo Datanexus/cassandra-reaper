00b9af84b090ef603f5b4925ee5ba92709024af1
mck <mick@semb.wever.org>
2017-02-24 19:54:03 +1100

    Download the cassandra-reaper jar file from the 0.4.0 github release. Add parameters for specifying cassandra seeds and cassandra cluster name.
    ref: https://github.com/Datanexus/dn-cassandra-reaper/pull/1

--- templates/start.sh
+++ templates/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-java -Xmx1g -jar /opt/cassandra-reaper/cassandra-reaper.jar server /opt/cassandra-reaper/reaper.yaml &
+java -Xmx1g -jar {{reaper_dir}}/target/cassandra-reaper-{{reaper_version}}-SNAPSHOT.jar server {{reaper_dir}}/reaper.yaml &
 
